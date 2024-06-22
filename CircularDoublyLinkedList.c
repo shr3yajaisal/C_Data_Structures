be9ac3ec3b0fd8f69865ce6184e8748b3972c4af
Shreya Jaisal <shreyajaisal90@gmail.com>
2024-06-22 23:16:06 +0530

    Update CircularDoublyLinkedList.c

--- CircularDoublyLinkedList.c
+++ CircularDoublyLinkedList.c
@@ -8,11 +8,79 @@ typedef struct node
     struct node* next;
 }node;
 
-node* AddToEmpty(int val)
+node* InsertAtBeginning(node* tail, int val)
 {
     node* temp = (node*)malloc(sizeof(node));
-    temp->data = val;
-    temp->prev = temp;
-    temp->next = temp;
-    return temp; 
+    if(tail == NULL)
+    {
+        temp->data = val;
+        temp->prev = temp;
+        temp->next = temp;
+    }
+    else
+    {
+        node* ptr = tail->next;
+        temp->data = val;
+        temp->prev = tail;
+        temp->next = ptr;
+        tail->next = temp;
+    }
+    return tail;
+}
+
+void display(node* tail)
+{
+    if(tail == NULL)
+    {
+        printf("Lnked list is empty!\n");
+    }
+    else
+    {
+        node* ptr = tail->next;
+        printf("%d\n", ptr->data);
+        ptr = ptr->next;
+
+        while(ptr != tail->next)
+        {
+            printf("%d\n", ptr->data);
+            ptr = ptr->next;
+        }
+    }
+}
+
+int main()
+{
+    node* tail = (node*)malloc(sizeof(node));
+    tail = NULL;
+
+    int op, val;
+    while(1)
+    {
+        printf("Enter 1 to Insert a node at the beginning\n");
+        printf("Enter 2 to print the linked list\n");
+        printf("Enter 3 to exit\n");
+
+        printf("Enter your option: ");
+        scanf("%d", &op);
+
+        switch(op)
+        {
+            case 1:
+            printf("Enter the value you want to insert at the beginning: ");
+            scanf("%d", &val);
+            tail = InsertAtBeginning(tail, val);
+            break;
+
+            case 2:
+            display(tail);
+            break;
+
+            case 3:
+            exit(1);
+            break;
+
+            default:
+            printf("Wrong input!\n");
+        }
+    }
 }
