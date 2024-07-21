384283135f4e9f7115a69dfa978bf96c3263cec5
Shreya Jaisal <shreyajaisal90@gmail.com>
2024-07-21 10:12:24 +0530

    Update 2SinglyLinkedList.c

--- 2SinglyLinkedList.c
+++ 2SinglyLinkedList.c
@@ -7,14 +7,16 @@ typedef struct node
     struct node *link;
 }node;
 
-node* AddToEmpty(node *head, int val){
+node* AddToEmpty(node *head, int val)
+{
     head = (node*)malloc(sizeof(node));
     head->data = val;
     head->link = NULL;
     return head;
 }
 
-int CountNodes(node *head){
+int CountNodes(node *head)
+{
     int count = 0;
     node *ptr = head;
     while(ptr != NULL)
@@ -26,7 +28,8 @@ int CountNodes(node *head){
     return count;
 }
 
-void PrintData(node *head){
+void PrintData(node *head)
+{
     if(head == NULL)
     {
         printf("Linked list is empty!\n");
@@ -43,7 +46,8 @@ void PrintData(node *head){
     }
 }
 
-node* InsertAtEnd(node *head, int val){
+node* InsertAtEnd(node *head, int val)
+{
     node *ptr = head;
     node *temp = (node*)malloc(sizeof(node));
     temp->data = val;
@@ -57,7 +61,8 @@ node* InsertAtEnd(node *head, int val){
     return head;
 }
 
-node* InsertAtBeginning(node *head, int val){
+node* InsertAtBeginning(node *head, int val)
+{
     node *ptr = (node*)malloc(sizeof(node));
     ptr->data = val;
     ptr->link = head;
@@ -66,7 +71,8 @@ node* InsertAtBeginning(node *head, int val){
     return head;
 }
 
-node* InsertInBetween(node *head, int pos, int val){
+node* InsertInBetween(node *head, int pos, int val)
+{
     int c = CountNodes(head);
 
     node *ptr = head;
@@ -98,7 +104,8 @@ node* InsertInBetween(node *head, int pos, int val){
     return head;
 }
 
-node* InsertionInSorted(node* head, int val){
+node* InsertionInSorted(node* head, int val)
+{
     node* temp = (node*)malloc(sizeof(node));
     temp->data = val;
     int key = val;
@@ -137,7 +144,8 @@ void DeleteFromBeginning(node **head){
     }
 }
 
-void DeleteFromEnd(node *head){
+void DeleteFromEnd(node *head)
+{
     if(head == NULL)
     {
         printf("Linked list is already empty!");
@@ -164,7 +172,8 @@ void DeleteFromEnd(node *head){
     } 
 }
 
-void DeleteInBetween(node* head){
+void DeleteInBetween(node* head)
+{
     int pos;
     node *ptr1 = head;
     node *ptr2 = NULL;
@@ -191,7 +200,8 @@ void DeleteInBetween(node* head){
     }
 }
 
-node* DeleteList(node* head){
+node* DeleteList(node* head)
+{
     node* temp = head;
     while(temp != NULL)
     {
@@ -202,7 +212,8 @@ node* DeleteList(node* head){
     return head;
 }
 
-node* CreateList(node* head){
+node* CreateList(node* head)
+{
     int n, val;
     printf("Enter the number of nodes you want to create: ");
     scanf("%d", &n);
@@ -227,7 +238,8 @@ node* CreateList(node* head){
     return head;
 }
 
-node* ReversingList(node* head){
+node* ReversingList(node* head)
+{
     node *prev = NULL;
     node* next = NULL;
 
@@ -242,7 +254,8 @@ node* ReversingList(node* head){
     return head;
 }
 
-int main(){
+int main()
+{
     node* head = (node*)malloc(sizeof(node));
     head = NULL;
 
