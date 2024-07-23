3c343bbdf28a881c46fa3235a079a2d9df4d6f54
Shreya Jaisal <shreyajaisal90@gmail.com>
2024-07-23 21:36:14 +0530

    Update 8ArrayImplementationOfStacks.c

--- 8ArrayImplementationOfStacks.c
+++ 8ArrayImplementationOfStacks.c
@@ -2,12 +2,36 @@
 #include <stdlib.h>
 #define MAX 4
 
-int top = -1;
 int stack[MAX];
+int top = -1;
+
+int IsFull()
+{
+    if(top == MAX - 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int IsEmpty()
+{
+    if(top == -1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
 
 void push(int data)
 {
-    if(top == MAX - 1)
+    if(IsFull())
     {
         printf("Stack overflow!\n");
     }
@@ -15,20 +39,77 @@ void push(int data)
     {
         top = top + 1;
         stack[top] = data;
-    }
+    } 
 }
 
 int pop()
 {
     int value;
-    if(top == -1)
+    if(IsEmpty()) 
     {
-        printf("Stack underflow\n");
+        printf("Stack underflow!\n");
     }
     else
     {
         value = stack[top];
         top = top - 1;
-    }
+    } 
     return value;
+}
+
+void print()
+{
+    int i;
+    if(top == -1)
+    {
+        printf("Stack underflow!\n");
+        return;
+    }
+    for(i=top; i>=0; i--)
+    {
+        printf("%d\t", stack[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int op, data;
+
+    while(1)
+    {
+        printf("Enter 1 to push\n");
+        printf("Enter 2 to pop\n");
+        printf("Enter 3 to print the stack\n");
+        printf("Enter 4 to exit\n");
+
+        printf("Enter your option: ");
+        scanf("%d", &op);
+
+        switch(op)
+        {
+            case 1:
+            printf("Enter the number you want to push: ");
+            scanf("%d", &data);
+            push(data);
+            break;
+
+            case 2:
+            pop();
+            break;
+
+            case 3:
+            print();
+            break;
+
+            case 4:
+            exit(1);
+            break;
+
+            default:
+            printf("Wrong input\n");
+            break;
+        }
+    }
+    return 0;
 }
