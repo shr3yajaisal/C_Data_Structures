7f3105a231624d472bff87e1102a4620568e8e99
Shreya Jaisal <shreyajaisal90@gmail.com>
2024-07-26 21:46:31 +0530

    Update BinarySearchTree.c

--- BinarySearchTree.c
+++ BinarySearchTree.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct treenode
-{
-    int data;
-    struct treenode *left;
-    struct treenode *right;
-}treenode;
+
 
 typedef struct binarytree
 {
