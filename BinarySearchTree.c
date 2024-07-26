10370f69a4cfede0fb83aeecb7f4877a244493cd
Shreya Jaisal <shreyajaisal90@gmail.com>
2024-07-26 21:46:50 +0530

    Update BinarySearchTree.c

--- BinarySearchTree.c
+++ BinarySearchTree.c
@@ -8,19 +8,7 @@ typedef struct treenode
     struct treenode *right;
 }treenode;
 
-typedef struct binarytree
-{
-    treenode* root;
-}binarytree;
 
-treenode* createtreenode(int data)
-{
-    treenode* newnode = (treenode*)malloc(sizeof(treenode*));
-    newnode->data = data;
-    newnode->left = NULL;
-    newnode->right = NULL;
-    return newnode;
-}
 
 treenode* insertnode(treenode *root, int val)
 {
