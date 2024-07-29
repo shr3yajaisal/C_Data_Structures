4a6a9dbdb8b13b6f65381e86152b2608f37fe88b
Shreya Jaisal <shreyajaisal90@gmail.com>
2024-07-29 21:45:26 +0530

    Update 1selfRefrentialStructures.c

--- 1selfRefrentialStructures.c
+++ 1selfRefrentialStructures.c
@@ -1,20 +1,3 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct student
-{
-    int roll;
-    char name[100];
-    struct student *p;
-}student;
-
-int main()
-{
-    student s1={127,"Shreya",NULL};
-    student s2={126,"Vaibhav", NULL};
-    s1.p=&s2;
-
-    printf("%d %s", s1.p->roll, s1.p->name);
-
-    return 0;
-}
